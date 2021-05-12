b85b8455241a6a40b1fcd1ac4569c85fb8435440
Dylan <dylansomeren@gmail.com>
2021-05-12 17:36:14 +0200

    Tijd+dag javascript gedaan(nu nog datum en tijd automatisch updaten)

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,14 @@ var date = new Date();
 function displayDay() {
 	var day = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
 	var dayName = day[date.getDay()];
-	document.getElementById('').innerHTML = dayName;
+	document.getElementById('showDay').innerHTML = dayName;
 }
 displayDay();
+
+function displayTime(){
+	var time = date.toLocaleTimeString();
+	document.getElementById('showTime').innerHTML = time;
+
+	
+}
+displayTime();
